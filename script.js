5075cf4728c1ea7dd7a6b307bde570773febe822
Anshul <avenger720720@gmail.com>
2025-07-12 18:21:37 +0530

    make style and working on functionality of colors

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let targetColor = ""
+let score = 0;
+let time = 30;
+let timer;
+let colors = ['red', 'orange', 'blue', 'green', 'black', 'grey', 'hotpink', 'purple', 'white', 'yellow', 'lightgreen', 'maroon', 'khaki', 'brown', 'navy', 'indigo']
+const grid = document.getElementById("grid")
 function startGame (){
-    
+    score = 0;
+    time = 30
 }
+
